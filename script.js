2ba516a616933cbefc8de09c7fb0db7b26504c1c
Enes Karakas <enes.coding@gmail.com>
2022-10-10 18:29:05 +0300

    till gallery everything is done

---
+++ script.js
@@ -0,0 +1,16 @@
+const iconsEls = document.querySelectorAll('.section-1-icons i');
+
+let i = 0;
+// setInterval(() => {
+//   const iconChange = document.querySelector('.section-1-icons i.change');
+
+//   if (i < iconsEls.length - 1) {
+//     iconChange.classList.remove('change');
+//     iconChange.nextElementSibling.classList.add('change');
+//     i++;
+//   } else {
+//     i = 0;
+//     iconChange.classList.remove('change');
+//     iconsEls[0].classList.add('change');
+//   }
+// }, 4000);
